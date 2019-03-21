c69b45afbe71995458a66128484f7a12763cb7c0
Assadbintahir <asadullah.09471@khi.iba.edu.pk>
2019-03-21 10:48:28 +0500

    again name check

--- server/express/routes.js
+++ server/express/routes.js
@@ -1,4 +1,7 @@
 module.exports.setup = (app, router) => {
+  router.get('/', (req, res, next) => {
+    res.status(200).json({name: 'asad'})
+  })
   require('./../api/test/routes').setup(router)
   app.use(router)
 }
