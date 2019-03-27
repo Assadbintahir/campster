249de72e902739dd093d5d91a57b0d114e3aa3a5
Assadbintahir <asadullah.09471@khi.iba.edu.pk>
2019-03-27 10:47:45 +0500

    #4: final react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,51 @@
 import React, { Component } from "react";
-import Routes from "./Routes";
+import {Route, Switch, Link } from "react-router-dom";
 
+const Home = () => <div>Home</div>;
+
+const About = () => <div>About</div>;
+
+const Code = () => <div>Code</div>;
+
+const Contact = () => <div>Contact</div>;
+
+const info = () => <div>info</div>;
+
+const MainMenu = () => {
+	return (
+	  <div>
+		<Link to="/">
+		  <button>home</button>
+		</Link>
+		<Link to="/about">
+		  <button>About</button>
+		</Link>
+		<Link to="/code">
+		  <button>code</button>
+		</Link>
+		<Link to="/contact">
+		  <button>contact</button>
+		</Link>
+		<Link to="/info">
+		  <button>info</button>
+		</Link>
+	  </div>
+	);
+  };
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      successMessage: ""
-    };
-    this.apiReq();
-  }
-  apiReq() {
-    fetch("/api/v1/test")
-      .then(response => response.json())
-      .then(state => {
-        this.setState({ successMessage: state.message });
-      });
-  }
+
   render() {
     return (
-        <div className="App">
-          <header className="App-header">
-            <p>Message from API server.</p>
-            <b>{this.state.successMessage}</b>
-          </header>
-          <div>
-            <Routes />
-          </div>
-        </div>
+        <>
+		<MainMenu />
+		<Switch>
+			<Route exact path="/" component={Home} />
+			<Route exact path="/about" component={About} />
+			<Route exact path="/code" component={Code} />
+			<Route exact path="/contact" component={Contact} />
+			<Route exact path="/info" component={info} />
+		</Switch>
+		</>
     );
   }
 }
